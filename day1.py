9cc539999eb4b5bca6e9152be7d574451e644765
Sumanth9148 <sumanthrn93@gmail.com>
2025-11-28 16:36:14 +0530

    two sum , three sum , valid anagram

--- day1.py
+++ day1.py
@@ -1,4 +1,5 @@
 #two sum
+
 # def two_sum(nums:list[int],target:int) -> list[int]:
 #     prevmap={}
 
@@ -10,33 +11,70 @@
 
 #     return []
 
-# print(two_sum(nums=[2,7,11,15], target=9))  
+# print(two_sum(nums=[2,7,11,15], target=9)) 
+
+# print(two_sum(nums=[3,2,4], target=6)) 
+
+
 
 
 #three sum
-def three_sum(nums: list[int]) -> list[list[int]]:
-    res=[]
-    nums.sort()
 
-    for i,num in enumerate(nums):
-        if i>0 and num==nums[i-1]:
-            continue
+# def three_sum(nums: list[int]) -> list[list[int]]:
+#     res=[]
+#     nums.sort()
 
-        l,r=i+1,len(nums)-1
+#     for i,num in enumerate(nums):
+#         if i>0 and num==nums[i-1]:
+#             continue
 
-        while l<r:
-            ts=num+nums[l]+nums[r]
+#         l,r=i+1,len(nums)-1
 
-            if ts>0:
-                r-=1
-            elif ts<0:
-                l+=1
-            else:
-                res.append([num,nums[l],nums[r]])
-                l+=1
-                while l<r and nums[l]==nums[l-1]:
-                    l+=1
+#         while l<r:
+#             ts=num+nums[l]+nums[r]
 
-    return res
+#             if ts>0:
+#                 r-=1
+#             elif ts<0:
+#                 l+=1
+#             else:
+#                 res.append([num,nums[l],nums[r]])
+#                 l+=1
+#                 while l<r and nums[l]==nums[l-1]:
+#                     l+=1
 
-print(three_sum(nums=[-1,0,1,2,-1,-4]))               
+#     return res
+
+# print(three_sum(nums=[-1,0,1,2,-1,-4])) 
+
+
+
+#valid anagram
+
+def valid_anagram(s: str, t: str) -> bool:
+    if len(s)!=len(t):
+        return False
+    
+    s_count={}
+
+    for ch in s:
+        s_count[ch]=s_count.get(ch,0)+1
+
+    #decrease that s_count now
+
+    for ch in t:
+        if ch not in s_count:
+            return False
+
+        s_count[ch]-=1
+
+        if s_count[ch]<0:
+            return False
+
+    return True
+
+
+
+print(valid_anagram(s="anagram", t="nagaram"))
+
+print(valid_anagram(s="rat", t="car"))
