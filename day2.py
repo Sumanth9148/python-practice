f693c291665c3a043cf5c7353e688544db63258c
Sumanth9148 <sumanthrn93@gmail.com>
2025-12-02 15:41:16 +0530

    rotate array , add two nums , kth ele

--- day2.py
+++ day2.py
@@ -26,22 +26,32 @@
 
 #add two numbers
 
-def add_two_numbers(l1: list[int], l2: list[int]) -> list[int]:
-    res=[]
-    i=0
-    j=0
-    carry=0
+# def add_two_numbers(l1: list[int], l2: list[int]) -> list[int]:
+#     res=[]
+#     i=0
+#     j=0
+#     carry=0
 
-    while i<len(l1) or j<len(l2) or carry:
-        x=l1[i] if i<len(l1) else 0
-        y=l2[j] if j<len(l2) else 0
+#     while i<len(l1) or j<len(l2) or carry:
+#         x=l1[i] if i<len(l1) else 0
+#         y=l2[j] if j<len(l2) else 0
 
-        tot=x+y+carry
-        res.append(tot%10)
-        carry=tot//10
+#         tot=x+y+carry
+#         res.append(tot%10)
+#         carry=tot//10
 
-        i+=1
-        j+=1
-    return res 
+#         i+=1
+#         j+=1
+#     return res 
 
-print(add_two_numbers(l1=[2,4,3], l2=[5,6,4]))   
+# print(add_two_numbers(l1=[2,4,3], l2=[5,6,4]))  
+
+
+#kth largest element
+
+def kth_largest_element_in_an_array(nums: list[int], k: int) -> int:
+    nums.sort(reverse=True)
+    return nums[k-1]
+
+print(kth_largest_element_in_an_array(nums=[3,2,1,5,6,4], k=2)) 
+print(kth_largest_element_in_an_array(nums=[3,2,3,1,2,4,5,5,6],k=4)) 
